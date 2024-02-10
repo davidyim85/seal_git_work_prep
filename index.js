34fd608367eac63d85fb49ec39adc9f4fb577dd6
yimd85 <yimd85@gmail.com>
2024-02-09 23:33:52 -0500

    merged

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {//;adf
-    res.send('hello world this is b!')//;adf
+    res.send('hello world this is b!!')//;adf
 });//test
 
 app.listen(process.env.PORT, () => console.log(`listening to port ${process.env.PORT}`));
