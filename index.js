0f1596449df0c79f49321ed99e279df9b6b6f7db
davidyim85 <119387051+davidyim85@users.noreply.github.com>
2024-02-09 23:32:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('hello world this is b!');
+app.get('/', (req, res) => {//;adf
+    res.send('hello world this is b!')//;adf
 });//test
 
 app.listen(process.env.PORT, () => console.log(`listening to port ${process.env.PORT}`));
